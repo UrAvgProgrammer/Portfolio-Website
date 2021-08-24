ebcc80895ef5b27e1d2caadd1ee1c3f592fef1b6
Jerald Talledo <jrldtlld@gmail.com>
2021-08-24 12:06:30 +0800

    fixed run flow

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 # from flask_bootstrap import Bootstrap
 
@@ -5,4 +7,8 @@ from flask import Flask
 app = Flask(__name__)
 # bootstrap = Bootstrap(app)
 
+app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['SECRET_KEY'] = os.urandom(24)
+
 from app import views
+
